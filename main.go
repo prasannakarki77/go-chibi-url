8577effbe25571dcd1d24646ee4523c020018838
prasannakarki77 <prasanna.karki77@gmail.com>
2024-11-07 21:04:59 +0545

    go server setup

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+type User struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
 
 func main() {
-	fmt.Println("Project setup!")
+	mux := http.NewServeMux()
+	serv := &http.Server{Handler: jsonContentTypeMiddleware(mux), Addr: ":8080"}
+	serv.ListenAndServe()
+}
+
+func jsonContentTypeMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("listening to server: 8080")
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
 }
