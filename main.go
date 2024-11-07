789cf5e12829b2d9da6c0e9d44d70d5ea15953ed
prasannakarki77 <prasanna.karki77@gmail.com>
2024-11-07 21:49:42 +0545

    database connection setup

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
 )
 
 type User struct {
@@ -12,7 +18,28 @@ type User struct {
 }
 
 func main() {
+	// connect to databases
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT, email TEXT)")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /users", func(w http.ResponseWriter, r *http.Request) {
+		user := User{ID: 1, Name: "hellllo", Email: "abc@gmail.com"}
+		if err := json.NewEncoder(w).Encode(user); err != nil {
+			http.Error(w, "Error encoding JSON", http.StatusInternalServerError)
+			return
+		}
+	})
+
 	serv := &http.Server{Handler: jsonContentTypeMiddleware(mux), Addr: ":8080"}
 	serv.ListenAndServe()
 }
